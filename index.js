364a490388a03d4465d67ae6e6a15f19fb76b73e
ellementul <ivanzamkov2014@gmail.com>
2024-04-25 09:52:15 +0500

    Delted graph

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ import { UnitedEventsEnv, Room } from '@ellementul/united-events-environment'
 import { Logging } from './logging'
 
 import { POSMember } from '@ellementul/pos-member'
-import { GraphRenderMember } from './members/GraphRender/index.js'
 import { UIMember } from './members/ui/index.js'
 
 const room = new Room
 room.addMember(POSMember)
-room.addMember(GraphRenderMember)
-// room.addMember(UIMember)
+room.addMember(UIMember)
 
 const env = new UnitedEventsEnv(room)
 env.baseUrl = "./"
